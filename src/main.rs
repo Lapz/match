5e3ebb0e641d0144cafcb5b5db2bdd6205b1b8ef
Lenard Pratt <l3np27@gmail.com>
2019-03-14 12:01:14 +0000

    implemented the simple partial evaluator

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::collections::{HashMap,HashSet};
+#![feature(slice_patterns)]
+use core::borrow::Borrow;
+use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 
-#[derive(Debug,Clone)]
-struct Context(Vec<(Constructor,Vec<TermDescription>)>);
+#[derive(Debug, Clone)]
+struct Context(Vec<(Constructor, Vec<TermDescription>)>);
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Value {
@@ -24,9 +26,9 @@ struct Constructor {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum TermDescription {
-    Pos(Constructor,Vec<TermDescription>),
+    Pos(Constructor, Vec<TermDescription>),
 
-    Neg(HashSet<Constructor>)
+    Neg(HashSet<Constructor>),
 }
 
 fn tt() -> Pattern {
@@ -43,7 +45,6 @@ enum Pattern {
     Con(Constructor, Vec<Pattern>), //e.g. List(10,Nil)
 }
 
-
 impl Constructor {
     pub fn new(name: &str, arity: i32, span: i32) -> Self {
         Self {
@@ -54,17 +55,23 @@ impl Constructor {
     }
 }
 
-
 impl TermDescription {
-    pub fn add_neg(&mut self,con:Constructor) {
-       match *self {
-           TermDescription::Neg(ref mut nonset) => {
-               if !nonset.contains(&con) && nonset.len()+1 < con.span as usize {
+    pub fn add_neg(&mut self, con: Constructor) {
+        match *self {
+            TermDescription::Neg(ref mut nonset) => {
+                if !nonset.contains(&con) && nonset.len() + 1 < con.span as usize {
                     nonset.insert(con);
-               }
-           },
-           _ => ()
-       }
+                }
+            }
+            _ => (),
+        }
+    }
+
+    fn get_dargs(&mut self) -> Vec<TermDescription> {
+        match self {
+            TermDescription::Neg(_) => vec![],
+            TermDescription::Pos(_, dargs) => dargs.clone(),
+        }
     }
 }
 
@@ -72,95 +79,111 @@ impl Context {
     pub fn new() -> Self {
         Self(Vec::new())
     }
-    pub fn augment(&mut self,desc:TermDescription) {
-
+    pub fn augment(&mut self, desc: TermDescription) {
         if self.0.is_empty() {
             return;
         }
 
-        let (con,mut args) = self.0.remove(0);
+        let (con, mut args) = self.0.remove(0);
 
         args.push(desc);
 
-        self.0.insert(0,(con,args));
+        self.0.insert(0, (con, args));
     }
 
     pub fn norm(&mut self) {
-        let (con,mut args) = self.0.remove(0);
+        let (con, mut args) = self.0.remove(0);
         args.reverse();
-        self.augment(TermDescription::Pos(con,args))
+        self.augment(TermDescription::Pos(con, args))
     }
 
     pub fn is_empty(&self) -> bool {
         self.0.is_empty()
     }
+
+    pub fn push(&mut self, item: (Constructor, Vec<TermDescription>)) {
+        self.0.push(item);
+    }
 }
 
-
-fn build_dsc(ctx:&mut Context,dsc:TermDescription,work:&mut Vec<(Pattern,Pattern,Vec<TermDescription>)>) -> TermDescription {
+fn build_dsc(
+    ctx: &mut Context,
+    dsc: TermDescription,
+    work: &mut Vec<(Vec<Pattern>, Vec<Pattern>, Vec<TermDescription>)>,
+) -> TermDescription {
     if ctx.is_empty() && work.is_empty() {
         dsc
-    }else {
-        let (con,mut args) = ctx.0.remove(0);
-        let (_,_,mut dargs) = work.remove(0);
+    } else {
+        let (con, mut args) = ctx.0.remove(0);
+        let (_, _, mut dargs) = work.remove(0);
 
-        dargs.insert(0,dsc);
+        dargs.insert(0, dsc);
         args.extend(dargs);
         args.reverse();
 
-
-        build_dsc(ctx,TermDescription::Pos(con,args),work)
+        build_dsc(ctx, TermDescription::Pos(con, args), work)
     }
-
 }
 /// Attempts to match the expression against each pattern from a rule in rules.
 /// It succeeds with the rhs if the rule matches; it fails otherwise
-fn fail(expr: Pattern, rules: &mut Vec<(Pattern, Pattern)>) -> Option<Pattern> {
+fn fail(
+    expr: Pattern,
+    dsc: TermDescription,
+    rules: &mut Vec<(Pattern, Pattern)>,
+) -> Option<Pattern> {
     if rules.is_empty() {
         return None;
     } else {
         let (pat1, rhs1) = rules.remove(0);
 
-        compile_match(pat1, expr, vec![], rhs1, rules)
+        compile_match(pat1, expr, dsc, &mut Context::new(), vec![], rhs1, rules)
     }
 }
 
 fn succeed(
-    mut work: Vec<(Pattern, Pattern)>,
+    ctx: &mut Context,
+    mut work: Vec<(Vec<Pattern>, Vec<Pattern>, Vec<TermDescription>)>,
     rhs: Pattern,
     rules: &mut Vec<(Pattern, Pattern)>,
 ) -> Option<Pattern> {
     if work.is_empty() {
         Some(rhs)
     } else {
-        let (pat1, obj1) = work.remove(0);
+        let mut work1 = work.remove(0);
 
-        compile_match(pat1, obj1, work, rhs, rules)
+        if work1.0.is_empty() || work1.1.is_empty() || work1.2.is_empty() {
+            ctx.norm();
+            succeed(ctx, work, rhs, rules)
+        } else {
+            let (pat1, obj1, desc1) = (work1.0.remove(0), work1.1.remove(0), work1.2.remove(0));
+            work.insert(0, work1);
+            compile_match(pat1, obj1, desc1, ctx, work, rhs, rules)
+        }
     }
 }
 
 enum MatchResult {
     Yes,
     No,
-    Maybe
+    Maybe,
 }
 
-fn static_match(pcon:Constructor,dsc:TermDescription) -> MatchResult {
+fn static_match(pcon: &Constructor, dsc: &TermDescription) -> MatchResult {
     match dsc {
-        TermDescription::Pos(ref c,ref descs) => {
-            if &pcon == c {
+        TermDescription::Pos(ref c, ref descs) => {
+            if pcon == c {
                 MatchResult::Yes
-            }else {
+            } else {
                 MatchResult::No
             }
-        },
+        }
 
         TermDescription::Neg(ref cons) => {
-            if cons.contains(&pcon) {
+            if cons.contains(pcon) {
                 MatchResult::No
-            }else if !cons.contains(&pcon) && pcon.span as usize == 1+cons.len() {
+            } else if !cons.contains(pcon) && pcon.span as usize == 1 + cons.len() {
                 MatchResult::Yes
-            }else {
+            } else {
                 MatchResult::Maybe
             }
         }
@@ -169,29 +192,45 @@ fn static_match(pcon:Constructor,dsc:TermDescription) -> MatchResult {
 
 fn compile_match(
     pat1: Pattern,
-    obj1: Pattern,
-    mut work: Vec<(Pattern, Pattern)>,
+    expr: Pattern,
+    mut dsc: TermDescription,
+    ctx: &mut Context,
+    mut work: Vec<(Vec<Pattern>, Vec<Pattern>, Vec<TermDescription>)>,
     rhs: Pattern,
     rules: &mut Vec<(Pattern, Pattern)>,
 ) -> Option<Pattern> {
     match pat1 {
-        Pattern::Var(_) => succeed(work, rhs, rules),
-        Pattern::Con(ref pcon, ref pargs) => match obj1 {
-            Pattern::Con(ref ocon, ref oargs) => {
-                if ocon == pcon {
-                    let both = oargs
-                        .clone()
-                        .into_iter()
-                        .zip(pargs.clone().into_iter())
-                        .collect::<Vec<(Pattern, Pattern)>>();
-                    work.extend(both);
+        Pattern::Var(_) => {
+            ctx.augment(dsc);
+            succeed(ctx, work, rhs, rules)
+        }
+        Pattern::Con(ref pcon, ref pargs) => match expr {
+            Pattern::Con(ref ocon, ref oargs) => match static_match(pcon, &dsc) {
+                MatchResult::Yes => {
+                    ctx.push((pcon.clone(), vec![]));
 
-                    succeed(work, rhs, rules)
-                } else {
-                    fail(obj1, rules)
+                    work.push((pargs.clone(), oargs.clone(), dsc.get_dargs()));
+
+                    succeed(ctx, work, rhs, rules)
                 }
+                MatchResult::No => fail(expr, build_dsc(ctx, dsc, &mut work), rules),
+                MatchResult::Maybe => {
+                    if ocon == pcon {
+                        ctx.push((pcon.clone(), vec![]));
+
+                        work.push((pargs.clone(), oargs.clone(), dsc.get_dargs()));
+
+                        succeed(ctx, work, rhs, rules)
+                    } else {
+                        dsc.add_neg(pcon.clone());
+                        fail(expr, dsc, rules)
+                    }
+                }
+            },
+            Pattern::Var(_) => {
+                ctx.augment(dsc);
+                succeed(ctx, work, rhs, rules)
             }
-            Pattern::Var(_) => succeed(work, rhs, rules),
         },
     }
 }
@@ -242,7 +281,14 @@ fn main() {
         ),
     ];
 
-    println!("{:#?}", fail(origobj, &mut allmrules))
+    println!(
+        "{:#?}",
+        fail(
+            origobj,
+            TermDescription::Neg(HashSet::new()),
+            &mut allmrules
+        )
+    )
 
     //example
     /*
@@ -306,10 +352,13 @@ mod test {
             ),
         ];
 
-
-        assert!(fail(origobj, &mut allmrules).is_some())
+        assert!(fail(
+            origobj,
+            TermDescription::Neg(HashSet::new()),
+            &mut allmrules
+        )
+        .is_some())
     }
-
 
     /// Fails because of missing rule
     #[test]
@@ -342,7 +391,13 @@ mod test {
             ),
         ];
 
-
-        assert_eq!(fail(origobj, &mut allmrules),None)
+        assert_eq!(
+            fail(
+                origobj,
+                TermDescription::Neg(HashSet::new()),
+                &mut allmrules
+            ),
+            None
+        )
     }
 }
